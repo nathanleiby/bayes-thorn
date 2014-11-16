48165c58a622823fe92614380c501a592c31c1a5
afishman <fishman.adam@gmail.com>
2014-11-16 10:17:41 -0800

    Added function to plot city data

--- webapp/scripts/controllers/cities.js
+++ webapp/scripts/controllers/cities.js
@@ -1,32 +1,46 @@
 'use strict';
 
 app.controller('citiesCtrl', function($scope){
-  function initialize() {
+  DataParser.readFile('data/links.csv').then(function(results) {
+    var points = _.map(results, function(line, i) {
+      return {
+        endPoints: [
+          new google.maps.maps.LatLng(results['lat1'], results['lng1']),
+          new google.maps.maps.LatLng(results['lat2'], results['lng2']),
+        ],
+        weight: results['weight']
+      }
+    });
+
+    function initialize(pointVals) {
       var mapOptions = {
-            zoom: 3,
-      center: new google.maps.LatLng(0, -180),
-      mapTypeId: google.maps.MapTypeId.TERRAIN
-    };
+        zoom: 3,
+        center: new google.maps.LatLng(0, -180),
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+      };
 
-        var map = new google.maps.Map(document.getElementById('map-canvas'),
-                mapOptions);
+      var map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
 
-          var flightPlanCoordinates = [
-      new google.maps.LatLng(37.772323, -122.214897),
-      new google.maps.LatLng(21.291982, -157.821856),
-      new google.maps.LatLng(-18.142599, 178.431),
-      new google.maps.LatLng(-27.46758, 153.027892)
-    ];
-  var flightPath = new google.maps.Polyline({
-        path: flightPlanCoordinates,
+      var flightPlanCoordinates = [
+        new google.maps.LatLng(37.772323, -122.214897),
+        new google.maps.LatLng(21.291982, -157.821856),
+        new google.maps.LatLng(-18.142599, 178.431),
+        new google.maps.LatLng(-27.46758, 153.027892)
+      ];
+
+      pointVals.foreach(function(point) {
+        var line = new google.maps.PolyLines({
+          path: point.endPoints,
           geodesic: true,
           strokeColor: '#FF0000',
           strokeOpacity: 1.0,
           strokeWeight: 2
+        });
+        line.setMap(map);
       });
-
-    flightPath.setMap(map);
-  }
-
-  initialize();
+    }
+    initialize(points);
+  });
 })
+
