d083ff463e60d6c3c8f52337a8e5388a2783a15c
afishman <fishman.adam@gmail.com>
2014-11-16 10:33:22 -0800

    Made the network lines work

--- webapp/scripts/controllers/cities.js
+++ webapp/scripts/controllers/cities.js
@@ -1,41 +1,36 @@
 'use strict';
 
-app.controller('citiesCtrl', function($scope){
-  DataParser.readFile('data/links.csv').then(function(results) {
+app.controller('citiesCtrl', function($scope, DataParser){
+  DataParser.readFile('data/links_trim.csv').then(function(results) {
     var points = _.map(results, function(line, i) {
+      console.log(Number(line.weight))
       return {
         endPoints: [
-          new google.maps.maps.LatLng(results['lat1'], results['lng1']),
-          new google.maps.maps.LatLng(results['lat2'], results['lng2']),
+          new google.maps.LatLng(line.lat1, line.lng1),
+          new google.maps.LatLng(line.lat2, line.lng2),
         ],
-        weight: results['weight']
+        weight: 10 * Number(line.weight)
       }
     });
 
+    console.log(points)
     function initialize(pointVals) {
       var mapOptions = {
-        zoom: 3,
-        center: new google.maps.LatLng(0, -180),
+        zoom: 4,
+        center: new google.maps.LatLng(39.8282, -98.5795),
         mapTypeId: google.maps.MapTypeId.TERRAIN
       };
 
       var map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
 
-      var flightPlanCoordinates = [
-        new google.maps.LatLng(37.772323, -122.214897),
-        new google.maps.LatLng(21.291982, -157.821856),
-        new google.maps.LatLng(-18.142599, 178.431),
-        new google.maps.LatLng(-27.46758, 153.027892)
-      ];
-
-      pointVals.foreach(function(point) {
-        var line = new google.maps.PolyLines({
+      _.each(pointVals, function(point) {
+        var line = new google.maps.Polyline({
           path: point.endPoints,
           geodesic: true,
           strokeColor: '#FF0000',
-          strokeOpacity: 1.0,
-          strokeWeight: 2
+          strokeOpacity: 0.5,
+          strokeWeight: point.weight
         });
         line.setMap(map);
       });
