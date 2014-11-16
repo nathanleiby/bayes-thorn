ae119e72bec49c8ee240dfdeeaf950fe928b886a
afishman <fishman.adam@gmail.com>
2014-11-16 10:37:52 -0800

    Added style for map

--- webapp/scripts/controllers/cities.js
+++ webapp/scripts/controllers/cities.js
@@ -18,7 +18,8 @@ app.controller('citiesCtrl', function($scope, DataParser){
       var mapOptions = {
         zoom: 4,
         center: new google.maps.LatLng(39.8282, -98.5795),
-        mapTypeId: google.maps.MapTypeId.TERRAIN
+        mapTypeId: google.maps.MapTypeId.TERRAIN,
+        styles: [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]
       };
 
       var map = new google.maps.Map(document.getElementById('map-canvas'),
@@ -28,8 +29,8 @@ app.controller('citiesCtrl', function($scope, DataParser){
         var line = new google.maps.Polyline({
           path: point.endPoints,
           geodesic: true,
-          strokeColor: '#FF0000',
-          strokeOpacity: 0.5,
+          strokeColor: '#FCFF00',
+          strokeOpacity: 0.7,
           strokeWeight: point.weight
         });
         line.setMap(map);
